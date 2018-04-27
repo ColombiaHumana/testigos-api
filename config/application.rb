0d0fd9fcbf4ff6089568a763af0ab27c2ba3f7d0
Mijail Rondon <mijail.rondon@gmail.com>
2018-04-27 12:01:36 -0500

    Deploy config

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,12 @@ module TestigosApi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    config.i18n.default_locale = :es
+    config.assets.precompile += %w( active_admin.js active_admin.css.scss )
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOW-FROM https://petro.com.co'
+    }
   end
 end
