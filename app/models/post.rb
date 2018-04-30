3c51ab7d04a833d2016328747cc02635e7326386
Mijail Rondon <mijail.rondon@gmail.com>
2018-04-30 00:20:49 -0500

    Add results model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :zone
   has_many :tables
   has_many :users, through: :tables
+  has_many :results, through: :tables
   delegate :municipality, to: :zone
   delegate :department, to: :municipality
 
