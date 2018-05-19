add6f3ee00039c456655ad3123abfca1b5084eec
Mijail Rondon <mijail.rondon@gmail.com>
2018-05-18 20:40:52 -0500

    Unify rescue record not found

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -3,7 +3,6 @@
 module Api
   # Report table controller
   class ReportsController < Api::ApiController
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
     def create
       @report = current_user.tables.find(params[:report][:table_id]).reports.where(issue: params[:report][:issue_id], user: current_user).first_or_create
       raise ActiveRecord::RecordNotFound unless @report
@@ -17,10 +16,6 @@ module Api
 
     def type
       params.require(:report).permit(:issue_id)
-    end
-
-    def record_not_found
-      head 404
     end
   end
 end
