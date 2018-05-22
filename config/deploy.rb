619667c30b702b327e2dc4b2389f292b1051334d
Mijail Rondon <mijail@riseup.net>
2018-05-22 16:35:10 -0500

    add users csv deploy link file

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,8 @@ set :deploy_to, '/app'
 set :ssh_options, verify_host_key: :secure
 set :linked_files, fetch(:linked_files, []).push(
   'config/database.yml',
-  'config/master.key'
+  'config/master.key',
+  'vendor/divipol/users.csv'
 )
 set :linked_dirs, fetch(:linked_dirs, []).push(
   'log',
