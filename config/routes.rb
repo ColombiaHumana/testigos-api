654f79fad73cc0a3cde13f356ae834faf69a90b0
Mijail Rondon <mijail@riseup.net>
2018-05-23 16:12:04 -0500

    Error 404

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,8 @@ Rails.application.routes.draw do
   authenticate :admin_user do
     mount Sidekiq::Web => 'admin/sidekiq'
   end
+  %w( 404 422 500 503 ).each do |code|
+    get code, :to => "errors#show", :code => code
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
