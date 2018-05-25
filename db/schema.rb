0aaf241ee206ace50674316772a5fa26c34e64e5
Mijail Rondon <mijail@riseup.net>
2018-05-25 10:00:04 -0500

    Email for new users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_23_051210) do
+ActiveRecord::Schema.define(version: 2018_05_25_144443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,6 +150,7 @@ ActiveRecord::Schema.define(version: 2018_05_23_051210) do
     t.bigint "post_id"
     t.boolean "online", default: false
     t.boolean "verified_email", default: false
+    t.string "phone"
     t.index ["document"], name: "index_users_on_document"
     t.index ["post_id"], name: "index_users_on_post_id"
   end
