ae67aa8f700a070732225e51fda64d6082c15c0a
Mijail Rondon <mijail@riseup.net>
2018-05-26 18:58:28 -0500

    new panel

--- app/admin/resumen.rb
+++ app/admin/resumen.rb
@@ -1,4 +1,61 @@
 ActiveAdmin.register_page "Resumen" do
   menu priority: 2
-  
+  content title: "Testigos de muestreo" do
+
+    columns do
+      column do
+        panel "Testigos de muestreo logueados por departamento" do
+          data = [
+            {
+              name: "Online",
+              data: Department.all.collect { |x| [x.name, x.users.where(online: true).count] }
+            },
+            {
+              name: "Offline",
+              data: Department.all.collect { |x| [x.name, x.users.where(online: false).count] }
+            }
+          ]
+          render 'bar_graph', { data: data }
+          # table_for Department.joins(:users).where('users.online = true').group(['departments.id', 'departments.name']).count.each do
+          #   column("Departamento") {| department | department[0][1] }
+          #   column("Logueados") { | department | department.last }
+          #   column("Porcentaje") { | department | "#{department.last * 100 / (Department.find(department[0][0]).users.count.nonzero? || 1)} %" }
+          # end
+        end # End panel
+      end
+    end
+
+    columns do
+      column do
+        panel "Testigos de muestreo logueados" do
+          columns do
+            column do
+              users = User.all
+              online = users.where(online: true).count
+              render 'logins', { number: online , icon: "user" }
+            end
+            column do
+              render 'logins', { number: "#{User.where(online: true).count * 100 / User.count}%", icon: "percent" }
+            end
+          end
+          columns do
+            column do
+              render 'login_chart'
+            end
+          end
+        end # End panel
+
+      end # End column
+
+      column do
+        panel "Testigos en el mapa" do
+          columns do
+            column do
+              render 'map_chart'
+            end
+          end
+        end
+      end # End column
+    end # End columns
+  end
 end
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,5 +1,6 @@
 class Result < ApplicationRecord
   after_commit :send_to_spek, on: :create
+  after_commit :calculate_coefficient, on: :create, if: -> (result) { result.table.sample? }
   belongs_to :table
   belongs_to :user, required: :false
 
@@ -28,4 +29,18 @@ class Result < ApplicationRecord
   def send_to_spek
     SpekJob.perform_later(self)
   end
+
+  def calculate_coefficient
+    scrutinized = self.table.department.scrutinized + self.votos_validos
+    self.table.department.update scrutinized: scrutinized
+    total_scrutinized = Department.sum(:scrutinized)
+    Department.all.each do | d |
+      percentage = (d.scrutinized / total_scrutinized.to_f)
+      coefficient = (d.weight / percentage)
+      d.update! percentage: percentage, coefficient: coefficient
+    end
+  end
 end
+
+# Department.update_all percentage: 1, coefficient: 1, scrutinized: 0
+# Result.destroy_all
