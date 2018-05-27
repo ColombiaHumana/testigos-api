3eec3de3cfb886ad053841050899d3b878def91b
Mijail Rondon <mijail@riseup.net>
2018-05-26 22:27:10 -0500

    move muestreo

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,4 @@ News.create([
   {title: "Prueba de noticia", content: "Este es el contenido de un mensaje de ejemplo. Hasta 250 caracteres.", public: true},
   {title: "Prueba de noticia 2", content: "Este es el contenido de un mensaje de ejemplo. Hasta 250 caracteres.", public: true}
 ])
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: 'administrador') if Rails.env.development?
