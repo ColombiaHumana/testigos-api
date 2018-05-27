aea05d56ce68b5e1c5c44de68ee29a07dd469fde
Mijail Rondon <mijail@riseup.net>
2018-05-27 10:45:45 -0500

    add lector abilities

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -5,12 +5,14 @@ class AdminAbility
     can :read, ActiveAdmin::Page, name: "Dashboard", namespace_name: "admin"
     if user.role?('lector')
       can :read, User
-      can :read, News
       can :read, Issue
       can :read, Report
+      can :read, ActiveAdmin::Page, name: 'Resumen', namespace_name: 'admin'
+      can :read, ActiveAdmin::Page, name: 'Incidencias', namespace_name: 'admin'
     end
 
     if user.role?('moderador')
+      can :read, ActiveAdmin::Page, name: 'Muestreo', namespace_name: 'admin'
       can :manage, News
     end
 
