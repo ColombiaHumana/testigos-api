ddd334341673bcdc5c54fd71686a2f860ddb7470
Mijail Rondon <mijail@riseup.net>
2018-05-26 21:38:21 -0500

    schema and git ignore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_150411) do
+ActiveRecord::Schema.define(version: 2018_05_27_020055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 2018_05_26_150411) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "sample", default: false
+    t.integer "order"
     t.index ["cod_table"], name: "index_tables_on_cod_table"
     t.index ["post_id"], name: "index_tables_on_post_id"
     t.index ["user_id"], name: "index_tables_on_user_id"
